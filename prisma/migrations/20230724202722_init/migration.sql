10bc5d612a79a74fd8e2ed0a8cf8b6dcc4797455
Lucas Fernandes Silva <lucasdotfs@gmail.com>
2023-07-24 18:18:07 -0300

    feat: add info on seed

---
+++ prisma/migrations/20230724202722_init/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "Booster" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "img" TEXT,
+    "unlock" TEXT NOT NULL,
+
+    CONSTRAINT "Booster_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CardOnBooster" (
+    "booster_id" INTEGER NOT NULL,
+    "card_id" INTEGER NOT NULL,
+    "rarity" TEXT,
+
+    CONSTRAINT "CardOnBooster_pkey" PRIMARY KEY ("booster_id","card_id")
+);
+
+-- CreateTable
+CREATE TABLE "Card" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "img" TEXT,
+    "description" TEXT,
+
+    CONSTRAINT "Card_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "CardOnBooster" ADD CONSTRAINT "CardOnBooster_booster_id_fkey" FOREIGN KEY ("booster_id") REFERENCES "Booster"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CardOnBooster" ADD CONSTRAINT "CardOnBooster_card_id_fkey" FOREIGN KEY ("card_id") REFERENCES "Card"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,10 +6,86 @@ import cards from "../prisma/data/tagForce1Cards";
 
 loadEnvironment()
 connectDb()
+type Dict = { [key: number]: string };
+
 
 async function main() 
 {
-    
+
+    for(let booster of boosters)
+    {
+        const boosterDB = await prisma.booster.findFirst({
+            where: {
+                name: booster.name
+            }
+        })
+
+        if(boosterDB !== null) continue;
+
+        const save = await prisma.booster.create({
+            data: {
+                name: booster.name,
+                unlock: booster.desc,
+                img: booster.image_url
+            }
+        });
+    }
+
+    for(let booster of Object.keys(cards))
+    {
+        // @ts-ignore
+        for(let card of cards[booster]) {
+            const [name, rarity] = Object.keys(card)
+
+            const boosterDb = await prisma.booster.findFirst({ where: { name: booster } })
+            if(boosterDb === null) continue;
+
+            let isCard = await prisma.card.findFirst({
+                where: {
+                    name
+                }
+            }) 
+            let id = 0;
+            if(isCard !== null)
+                id = isCard.id;
+ 
+            if(isCard === null) {
+                const cardDb = await prisma.card.create({
+                    data: {
+                        name,
+                        description: card[name]["desc"],
+                        img: card[name]["image_url"]
+                    }
+                })
+                id = cardDb.id
+            }
+
+            
+            
+            let isCardOnBooster = await prisma.cardOnBooster.findFirst({
+                where: {
+                    card: {
+                        name,
+                    },
+                    booster: {
+                        name: booster
+                    }
+                }
+            })
+
+            if(isCardOnBooster) continue;
+
+            await prisma.cardOnBooster.create({
+                data: {
+                    card_id: id,
+                    booster_id: boosterDb.id,
+                    rarity: card[rarity]
+                }
+            })
+        }
+    }
+
+    // EXCEPTIONS BOOSTERS
 
 }
 
